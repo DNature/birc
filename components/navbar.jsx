abc4f6311b2c5114e6e6962bf0eaa2116b9e7b3a
DNature <divinehycenth@outlook.com>
2020-04-16 13:38:24 +0100

    added brush and completed about page

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
-import PropTypes from "prop-types";
-import { IoMdClose } from "react-icons/io";
-import { FiMenu } from "react-icons/fi";
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { IoMdClose } from 'react-icons/io';
+import { FiMenu } from 'react-icons/fi';
 
-import CustomLink from "./customLink";
-import { useRouter } from "next/dist/client/router";
+import CustomLink from './customLink';
+import { useRouter } from 'next/dist/client/router';
 
 const Navbar = ({ pathname, routes }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,9 +19,9 @@ const Navbar = ({ pathname, routes }) => {
         setNavBackground(show);
       }
     };
-    document.addEventListener("scroll", handleScroll);
+    document.addEventListener('scroll', handleScroll);
     return () => {
-      document.removeEventListener("scroll", handleScroll);
+      document.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
@@ -33,31 +33,30 @@ const Navbar = ({ pathname, routes }) => {
     <>
       <div
         className={`py-4 justify-center z-20 fixed top-0 left-0 w-full hidden sm:flex ${
-          navBackground && "bg-white shadow"
+          navBackground && 'bg-white shadow-lg brush'
         }`}
-        style={{ borderBottom: !navBackground && "2px solid #d3d4d4" }}
       >
-        <nav className="flex content-center ">
-          <CustomLink href="/" className="my-auto">
+        <nav className='flex content-center '>
+          <CustomLink href='/' className='my-auto'>
             <img
-              src="/logo.svg"
-              alt="Logo"
+              src='/logo.svg'
+              alt='Logo'
               className={`${
-                navBackground ? "w-16" : "w-24"
+                navBackground ? 'w-16' : 'w-24'
               } mr-10  transition-all duration-200`}
             />
           </CustomLink>
           {routes.map(({ path, name }) => (
             <CustomLink
               key={name}
-              className="py-2 mr-6 text-gray-75 font-semibold block"
+              className='py-2 mr-6 text-gray-75 font-semibold block'
               href={path}
             >
               <>
                 {name}
 
-                {pathname?.split("/")[1] == path.split("/")[1] && (
-                  <span className="block mt-3 w-5 h-1 mx-auto text-center bg-primary-100 rounded"></span>
+                {pathname?.split('/')[1] == path.split('/')[1] && (
+                  <span className='block mt-3 w-5 h-1 mx-auto text-center bg-primary-100 rounded'></span>
                 )}
               </>
             </CustomLink>
@@ -67,23 +66,23 @@ const Navbar = ({ pathname, routes }) => {
 
       <div
         className={`z-20 fixed top-0 left-0 w-full py-0 flex flex-col sm:hidden ${
-          navBackground && "bg-primary-100 shadow"
+          navBackground && 'bg-primary-100 shadow'
         }`}
       >
-        <div className="flex justify-between p-4">
-          <CustomLink href="/">
+        <div className='flex justify-between p-4'>
+          <CustomLink href='/'>
             <img
-              src={`${!navBackground ? "/logo.svg" : "/logo-white.svg"}`}
-              alt="Logo"
+              src={`${!navBackground ? '/logo.svg' : '/logo-white.svg'}`}
+              alt='Logo'
               className={`${
-                navBackground ? "w-16" : "w-24"
+                navBackground ? 'w-16' : 'w-24'
               } transition-all duration-200`}
             />
           </CustomLink>
           <button onClick={() => setIsOpen(true)}>
             <FiMenu
               className={`text-2xl my-auto ${
-                navBackground ? "text-white" : "text-gray-100"
+                navBackground ? 'text-white' : 'text-gray-100'
               }`}
             />
           </button>
@@ -92,27 +91,27 @@ const Navbar = ({ pathname, routes }) => {
 
       <nav
         data-isopen={isOpen}
-        className="fixed top-0 left-0 w-full z-50 py-6 h-screen overflow-hidden bg-primary-100 translate-x-56 flex flex-col"
+        className='fixed top-0 left-0 w-full z-50 py-6 h-screen overflow-hidden bg-primary-100 translate-x-56 flex flex-col'
         style={{
-          transform: isOpen ? "translateX(0)" : "translateX(-100%)",
-          transition: "transform 0.25s ease-in-out",
+          transform: isOpen ? 'translateX(0)' : 'translateX(-100%)',
+          transition: 'transform 0.25s ease-in-out',
         }}
       >
-        <div className="flex px-4 justify-between">
-          <CustomLink href="/">
+        <div className='flex px-4 justify-between'>
+          <CustomLink href='/'>
             <img
               src={`/logo-white.svg`}
-              alt="Logo"
-              className="w-16"
+              alt='Logo'
+              className='w-16'
               onClick={handleClose}
             />
           </CustomLink>
           <button onClick={handleClose}>
-            <IoMdClose className="text-white text-2xl my-auto" />
+            <IoMdClose className='text-white text-2xl my-auto' />
           </button>
         </div>
-        <hr className="my-6 text-white" />
-        <span className="flex-1">
+        <hr className='my-6 text-white' />
+        <span className='flex-1'>
           {routes.map(({ path, name, icon }) => (
             <CustomLink
               key={name}
@@ -123,16 +122,16 @@ const Navbar = ({ pathname, routes }) => {
                 {icon}
                 {name}
 
-                {pathname?.split("/")[1] == path.split("/")[1] && (
-                  <span className="block mt-3 w-full h-1 mx-auto text-center bg-white rounded bg-gray-400"></span>
+                {pathname?.split('/')[1] == path.split('/')[1] && (
+                  <span className='block mt-3 w-full h-1 mx-auto text-center bg-white rounded bg-gray-400'></span>
                 )}
               </>
             </CustomLink>
           ))}
         </span>
 
-        <div className="col-span-6 text-center mt-6">
-          <p className="text-gray-25 text-sm">
+        <div className='col-span-6 text-center mt-6'>
+          <p className='text-gray-25 text-sm'>
             &copy; Copyright 2020. birc.com
           </p>
         </div>
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import MainLayout from "../components/layouts/layout";
+import React, { useState } from 'react';
+import MainLayout from '../components/layouts/layout';
 
 const Vision = () => (
-  <div className="bg-gray-25 p-4 rounded rounded-tl-none">
-    <p className="text-lg">
+  <div className='bg-gray-25 p-4 rounded rounded-tl-none'>
+    <p className='text-lg'>
       Our vision is to maximise opportunities, expand horizons and place our
       clients in market leadership within their various businesses.
     </p>
@@ -11,8 +11,8 @@ const Vision = () => (
 );
 
 const Mission = () => (
-  <div className="bg-gray-25 p-4 rounded">
-    <p className="text-lg">
+  <div className='bg-gray-25 p-4 rounded'>
+    <p className='text-lg'>
       We seek to always provide our clients with customised services by
       transforming and improving businesses and individuals, by the quality
       service we deliver, which is translated to revenue generated.
@@ -40,44 +40,44 @@ const About = () => {
   };
 
   return (
-    <MainLayout pathname="/about">
-      <div className="w-full relative mt-24">
+    <MainLayout pathname='/about' className='relative'>
+      <div className='w-full relative mt-24'>
         <div
-          className="absolute top-0 left-0 bottom-0 right-0"
+          className='absolute top-0 left-0 bottom-0 right-0'
           style={{
             backgroundImage:
               "url('https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundAttachment: "fixed",
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundAttachment: 'fixed',
             zIndex: 1,
           }}
         />
         <div
-          className="bg-black top-0 left-0 right-0 bottom-0 absolute opacity-75"
+          className='bg-black top-0 left-0 right-0 bottom-0 absolute opacity-75'
           style={{ zIndex: 2 }}
         />
 
         <div
-          className="container py-24 text-white mx-auto relative h-full max-w-2xl mx-auto py-8 "
+          className='container py-24 text-white mx-auto relative h-full max-w-2xl mx-auto py-8 '
           style={{ zIndex: 3 }}
         >
-          <h2 className="text-white text-2xl font-bold">ABOUT US</h2>
-          <div className="my-4 h-2 bg-gray-25 lg:w-1/6 w-2/6" />
+          <h2 className='text-white text-2xl font-bold'>ABOUT US</h2>
+          <div className='my-4 h-2 bg-gray-25 lg:w-1/6 w-2/6' />
         </div>
       </div>
 
       {/* About Birc */}
-      <div className="">
-        <div className="container max-w-2xl mx-auto py-8">
-          <div className="mb-12">
-            <h2 className="text-xl font-bold text-gray-100">
+      <div className='relative'>
+        <div className='container max-w-2xl mx-auto py-8 after'>
+          <div className='mb-12'>
+            <h2 className='text-xl font-bold text-gray-100'>
               ABOUT <b>BIRC</b> LTD
             </h2>
 
-            <div className="my-4 h-1 bg-gray-100 lg:w-1/6 w-2/6"></div>
+            <div className='my-4 h-1 bg-gray-100 lg:w-1/6 w-2/6'></div>
           </div>
-          <p className="text-gray-75 mb-6 text-md">
+          <p className='text-gray-75 mb-6 text-md'>
             <b>BIRC</b> is a multi-disciplinary consultancy company with core
             consultants as staff. At <b>BIRC</b> we rely upon the expertise of
             these consultants who are not only academically sound but also
@@ -87,14 +87,14 @@ const About = () => {
             various organisations, Government and business schools, both locally
             and internationally.
           </p>
-          <div className={`${!open && "hidden"} `} data-open={open}>
-            <p className="text-gray-75 mb-6 text-md">
+          <div className={`${!open && 'hidden'} `} data-open={open}>
+            <p className='text-gray-75 mb-6 text-md'>
               <b>BIRC</b> provide solutions to clients in various sectors of the
               We have a strong team, comprising individuals with unique
               competencies in each of our practice areas.
             </p>
 
-            <p className="text-gray-75 mb-6 text-md">
+            <p className='text-gray-75 mb-6 text-md'>
               Using technology, our methodology is premised on simplicity,
               learning with ease, best-practice localization, quality,
               value-derivation, practicality and uniqueness. These have endeared
@@ -102,7 +102,7 @@ const About = () => {
               several years.
             </p>
 
-            <p className="text-gray-75 mb-6 text-md">
+            <p className='text-gray-75 mb-6 text-md'>
               With our focus on leadership in the Market, we currently have our
               presence in Abuja, Kaduna and Jos. This provides us with the
               requisite proximity to our clients.
@@ -110,15 +110,15 @@ const About = () => {
           </div>
 
           <button className={`text-primary-100 font-bold`} onClick={toggleOpen}>
-            {open ? "Read less" : "Read more..."}
+            {open ? 'Read less' : 'Read more...'}
           </button>
-          <hr className="my-4 text-gray-25" />
+          <hr className='my-4 text-gray-25' />
 
           {/* Mission and vision */}
-          <div className="mt-12">
+          <div className='mt-12'>
             <button
               className={`md:text-lg font-bold text-gray-100 px-4 py-2 rounded-tr ${
-                page !== pages.length - 1 ? "bg-gray-25" : "hover:bg-gray-15"
+                page !== pages.length - 1 ? 'bg-gray-25' : 'hover:bg-gray-15'
               }`}
               onClick={prevPage}
             >
@@ -126,7 +126,7 @@ const About = () => {
             </button>
             <button
               className={`md:text-lg font-bold text-gray-100 px-4 rounded-tl rounded-tr py-2 ${
-                page === pages.length - 1 ? "bg-gray-25" : "hover:bg-gray-15"
+                page === pages.length - 1 ? 'bg-gray-25' : 'hover:bg-gray-15'
               }`}
               onClick={nextPage}
             >
@@ -138,53 +138,53 @@ const About = () => {
         </div>
 
         {/* Partners */}
-        <div className="bg-gray-15">
-          <div className="container mx-auto max-w-2xl py-20">
-            <div className="mb-20">
-              <h2 className="text-xl font-bold text-gray-100">
+        <div className='bg-gray-15 relative'>
+          <div className='container mx-auto max-w-2xl py-20 after before'>
+            <div className='mb-20'>
+              <h2 className='text-xl font-bold text-gray-100'>
                 WE ONLY PARTNER WITH TOP CLASS FIRMS
               </h2>
 
-              <div className="my-4 h-2 bg-gray-100 lg:w-1/6 w-2/6"></div>
+              <div className='my-4 h-2 bg-gray-100 lg:w-1/6 w-2/6'></div>
             </div>
 
-            <div className="md:flex">
-              <div className="md:flex-shrink-0">
+            <div className='md:flex'>
+              <div className='md:flex-shrink-0'>
                 <img
-                  className="rounded md:w-56"
-                  src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
-                  alt="Woman paying for a purchase"
+                  className='rounded md:w-56'
+                  src='https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80'
+                  alt='Woman paying for a purchase'
                 />
               </div>
-              <div className="mt-4 md:mt-0 md:ml-6">
+              <div className='mt-4 md:mt-0 md:ml-6'>
                 <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline focus:text-primary-100"
+                  href='#'
+                  className='block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline focus:text-primary-100'
                 >
                   IT Support LTD.
                 </a>
-                <p className="mt-2 text-gray-600">
+                <p className='mt-2 text-gray-600'>
                   Getting a new business off the ground is a lot of hard work.
                   Here are five ideas you can use to find your first customers.
                 </p>
               </div>
             </div>
-            <div className="md:flex mt-6">
-              <div className="md:flex-shrink-0">
+            <div className='md:flex mt-6'>
+              <div className='md:flex-shrink-0'>
                 <img
-                  className="rounded md:w-56"
-                  src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                  alt="Woman paying for a purchase"
+                  className='rounded md:w-56'
+                  src='https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+                  alt='Woman paying for a purchase'
                 />
               </div>
-              <div className="mt-4 md:mt-0 md:ml-6">
+              <div className='mt-4 md:mt-0 md:ml-6'>
                 <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+                  href='#'
+                  className='block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline'
                 >
                   Finding customers for your new business
                 </a>
-                <p className="mt-2 text-gray-600">
+                <p className='mt-2 text-gray-600'>
                   Getting a new business off the ground is a lot of hard work.
                   Here are five ideas you can use to find your first customers.
                 </p>
@@ -194,21 +194,201 @@ const About = () => {
         </div>
 
         {/* Management */}
-        <div className="">
-          <div className="container mx-auto max-w-2xl py-20">
-            <div className="mb-10">
-              <h2 className="text-xl font-bold text-gray-100">
+        <div className='relative'>
+          <div className='container mx-auto max-w-2xl py-20 after'>
+            <div className='mb-10'>
+              <h2 className='text-xl font-bold text-gray-100'>
                 OUR MANAGEMENT
               </h2>
 
-              <div className="my-4 h-2 bg-gray-100 lg:w-1/6 w-2/6"></div>
+              <div className='my-4 h-2 bg-gray-100 lg:w-1/6 w-2/6'></div>
             </div>
 
-            <div className="">
-              <p className="text-lg font-semibold">
+            <div className=''>
+              <p className='text-lg font-semibold'>
                 BIRC is under the Leadership of four professionals as Directors
                 with:
               </p>
+
+              <ul className='mt-10 pl-6'>
+                <li className='mb-6'>
+                  <b>- Academic Pedigree: </b> ABU, UNIJOS University of
+                  Wolverhampton , SMC University, Law school
+                </li>
+
+                <li className='mb-6'>
+                  <b>- Professional Pedigree: </b>UBA, Access Bank, Leadway
+                  Pensure, Ernst & Young, Pitney Bowes etc
+                </li>
+                <li className='mb-6'>
+                  <b>- Stability and Conservatism </b>
+                </li>
+                <li className='mb-6'>
+                  <b>- Driven by Excellence </b>
+                </li>
+                <li className='mb-6'>
+                  <b>- Technical soundness in our core competence areas: </b>
+                  Human Resources Consulting , Strategy , Capacity Building (
+                  Training) & Risk Management
+                </li>
+              </ul>
+            </div>
+
+            <h2 className='text-xl mt-20 font-bold text-gray-100'>
+              MEET THE TEAM
+            </h2>
+            <div className='my-4 h-2 bg-gray-100 lg:w-1/6 w-2/6'></div>
+
+            {/* Dahiru */}
+            <div className=''>
+              <div className='md:flex mt-6'>
+                <div className='md:flex-shrink-0'>
+                  <img
+                    className='rounded md:w-56'
+                    src='/images/users/rector.jpg'
+                    alt='Woman paying for a purchase'
+                  />
+                </div>
+                <div className='mt-4 md:mt-0 md:ml-6 lg:pt-12'>
+                  <a
+                    href='#'
+                    className='block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline'
+                  >
+                    Dr. Dahiru Sani
+                  </a>
+                  <p className='mt-2 text-gray-600'>
+                    Rector and DG Kaduna Business School, Enterprise Institute..
+                    Holds a PhD in Finance. Passionate about empowering the next
+                    generation and making the world a better place.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <hr className='my-20 text-gray-25' />
+            {/* Henry */}
+            <div className=''>
+              <div className='md:flex mt-6 flex-col-reverse md:flex-row'>
+                <div className='mt-4 md:mt-0 md:ml-6 lg:pt-6 lg:pr-6 md:text-right'>
+                  <a
+                    href='#'
+                    className='block mt-1 text-lg  leading-tight font-semibold text-gray-900 hover:underline'
+                  >
+                    Henry Bada.
+                  </a>
+                  <p className='mt-2 text-gray-600 '>
+                    Principal Consultant (IM) BIRC, Regional Business
+                    Adviser/Analyst. Hpierson Associates. Associate Consultant
+                    KBS. Holds a Masters in Strategic Information Management.
+                    Passionate about Innovative Entrepreneurship
+                  </p>
+                </div>
+                <div className='md:flex-shrink-0'>
+                  <img
+                    className='rounded md:w-56 mx-auto md:mx-px mt-6 md:mt-0'
+                    src='/images/users/henry.jpg'
+                    alt='Woman paying for a purchase'
+                  />
+                </div>
+              </div>
+            </div>
+
+            <hr className='my-20 text-gray-25' />
+
+            {/* Dahiru */}
+            <div className=''>
+              <div className='md:flex mt-6'>
+                <div className='md:flex-shrink-0'>
+                  <img
+                    className='rounded md:w-56'
+                    src='/images/users/ibrahim.jpg'
+                    alt='Woman paying for a purchase'
+                  />
+                </div>
+                <div className='mt-4 md:mt-0 md:ml-6 lg:pt-12'>
+                  <a
+                    href='#'
+                    className='block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline'
+                  >
+                    Ibrahim Bonet
+                  </a>
+                  <p className='mt-2 text-gray-600'>
+                    Regional Manager Leadway, Director Business Development
+                    BIRC. Seasoned Banker with an MBA in Business
+                    Administration. Passionate about Risk Management and
+                    Strategy.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Choose us */}
+        <div className='bg-gray-15'>
+          <div className='container mx-auto max-w-2xl py-20'>
+            <div className='mb-12'>
+              <h2 className='text-xl font-bold text-gray-100'>
+                CHOOSE US FOR OUR:
+              </h2>
+
+              <div className='my-4 h-2 bg-gray-100 lg:w-1/6 w-2/6'></div>
+            </div>
+
+            <div className='my-12'>
+              <div>
+                <h3 className='font-semibold text-lg'>
+                  {' '}
+                  1. History of Performance
+                </h3>
+                <div className='my-6 pl-6'>
+                  <p className='mb-6'>
+                    - Within a decade we have delivered superior solutions to
+                    over 30 clients.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <h3 className='font-semibold text-lg'>
+                  {' '}
+                  2. Quality of our Technical Team
+                </h3>
+                <div className='my-6 pl-6'>
+                  <p className='mb-6'>
+                    - Our firm has a unique team of consultants across our
+                    various practice areas with extensive exposure and
+                    experience in their specific areas of competence. They are
+                    experienced professionals with diverse backgrounds who serve
+                    in our practice areas.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <h3 className='font-semibold text-lg'>
+                  {' '}
+                  3. A Blend of Local and International Best Practice Experience
+                </h3>
+                <div className='my-6 pl-6'>
+                  <p className='mb-6'>
+                    - Our team brings to each project, the best of global and
+                    local exposures from our consultants and the practicability
+                    of same, within our environment.
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <h3 className='font-semibold text-lg'>
+                  4. Our Professionalism
+                </h3>
+                <div className='my-6 pl-6'>
+                  <p className='mb-6'>
+                    - We abide by the principles of Consultancy and always
+                    ensure superior value for our clients in meeting their
+                    business goals.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
